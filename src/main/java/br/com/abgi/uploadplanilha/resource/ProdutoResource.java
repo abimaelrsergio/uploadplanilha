d96671319d98128740f5730041a3a874c6e09ba3
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-10-30 21:48:49 -0300

    atualizacao

--- src/main/java/br/com/abgi/uploadplanilha/resource/ProdutoResource.java
+++ src/main/java/br/com/abgi/uploadplanilha/resource/ProdutoResource.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
